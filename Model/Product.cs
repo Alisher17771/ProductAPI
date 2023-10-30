df653415e1d96c41acc4a0159049a85f354956c6
Alisher17771 <aliserozodov@gmail.com>
2023-10-30 17:48:49 +0500

    Добавьте файлы проекта.

---
+++ Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductAPI.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public int categoryid { get; set; }
+
+    }
+}
